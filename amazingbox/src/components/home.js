c0561a4602df972f3da051555881bcb529cf9e94
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2019-04-25 19:04:12 +0200

    simple getFiles function

--- amazingbox/src/components/home.js
+++ amazingbox/src/components/home.js
@@ -12,10 +12,32 @@ const Home = () => {
       updateUserToken(token);
     });
 
+    getFiles();
+
     return () => {
       subscription.unsubscribe();
     };
   }, []);
+
+  function getFiles(path = "") {
+    let dbx = new Dropbox.Dropbox({ accessToken: userToken });
+
+    dbx
+      .filesListFolder({ path })
+      .then(function(response) {
+        let files = response.entries;
+        console.log(files);
+
+        for (let file of files) {
+          if (file[".tag"] === "folder") {
+            getFiles(file.path_lower);
+          }
+        }
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  }
 
   function logOut() {
     let dbx = new Dropbox.Dropbox({ accessToken: userToken });
