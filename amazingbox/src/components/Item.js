2fa008293fccc28da2efe29aa6a38968ee482596
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-04-30 10:37:04 +0200

    pathing added - jonathan & Yaro

--- amazingbox/src/components/Item.js
+++ amazingbox/src/components/Item.js
@@ -12,7 +12,7 @@ function Item(props) {
     return (
         <>
             <td className="material-icons">{type === "folder" ? "folder" : "insert_drive_file"}</td>
-            <td>{type === "folder" ? <Link to={`${file.path_display}`}>{file.name}</Link> : file.name}</td>
+            <td>{type === "folder" ? <Link to={`/home${file.path_display}`}>{file.name}</Link> : file.name}</td>
             <td>{type !== "folder" ? getTime(file.server_modified) : null}</td>
             <td>{type !== "folder" ? bytesToSize(file.size) : null}</td>
             <td><button onClick={()=>!showMenu? updateShowMenu(true) : updateShowMenu(false)} className="material-icons">more_horiz</button></td>
