dc125221ad39e096050ccbe22b92651e49e787e9
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-05-02 22:17:20 +0200

    create folder is now alive fully completed 100%  gött - jonathan

--- amazingbox/src/components/addFileAndFolder.js
+++ amazingbox/src/components/addFileAndFolder.js
@@ -1,5 +1,4 @@
 import React, {useReducer, useState} from "react";
-import { Redirect } from "react-router-dom";
 import Dropbox from "dropbox";
 import fetch from "isomorphic-fetch";
 import { token$, updateToken } from "../store/authToken";
@@ -12,15 +11,29 @@ function reducer(state, action){
         return{
             ...state,
             showMenu: !state.showMenu? state.showMenu = true : state.showMenu = false,
+            showCreateFolder: state.showMenu = false,
         };
+
+        case "show_create_folder":
+            console.log(state);
+        return{
+            ...state,
+            showCreateFolder: state.showCreateFolder = true,
+        }
+
+        case "cancle_folder":
+        return{
+            ...state,
+            showCreateFolder: state.showCreateFolder = false,
+        }
     }
 }
 
 
 
 let AddFileButton = (props)=>{
-    const [state, dispatch] = useReducer(reducer, {showMenu: false, showCreateFolder: false});
-    const [userToken, updateUserToken] = useState(token$.value);
+    const [state, dispatch] = useReducer(reducer, {showMenu: false, showCreateFolder: false, inputValue: ""});
+    const [userToken, updateUserToken] = useState(token$.value); 
 
 
     function onFileChange(e){ //flytta till reducer
@@ -45,10 +58,10 @@ let AddFileButton = (props)=>{
         }
     }
 
-    function createFolder(){ //flytta till reducer
+    function createFolder(){ //flytta till reducer, får fel i reducer, kolla med andreas
         let dbx = new Dropbox.Dropbox({ fetch, accessToken: userToken });
         dbx
-        .filesCreateFolder({path: `${props.path}/${/*folder name*/}`})
+        .filesCreateFolder({path: `${props.path}/${state.inputValue}`}) //add folder name 
             .then((response)=>{
                 console.log(response);
                 props.updateFiles();
@@ -61,15 +74,15 @@ let AddFileButton = (props)=>{
             {state.showCreateFolder? 
             <div className="modal__shadow">
                 <div className="modal__shadow__container">
-                    <input type="text" onChange={dispatch({type: "input_change", value: e.target.value})} required></input>
+                    <input type="text" onChange={(e)=> {state.inputValue = e.target.value; console.log(state.inputValue)}} required></input>
                     <span className="modal__shadow__container__buttonBox">
-                        <button className="modal__shadow__container__buttonBox__button" onClick={dispatch({type: "create_folder"})}>Create</button>
-                        <button className="modal__shadow__container__buttonBox__button" onClick={dispatch({type: "cancle_folder"})}>Cancle</button>
+                        <button className="modal__shadow__container__buttonBox__button" onClick={createFolder}>Create</button>
+                        <button className="modal__shadow__container__buttonBox__button" onClick={()=>dispatch({type: "cancle_folder"})}>Cancle</button>
                     </span>
                 </div>
             </div> : null}
 
-            {state.showMenu? <div><input type="file" multiple onChange={onFileChange}></input><button>Create folder</button></div>: null}
+            {state.showMenu? <div><input type="file" multiple onChange={onFileChange}></input><button onClick={()=>dispatch({type: "show_create_folder"})}>Create folder</button></div>: null}
             <button onClick={()=>dispatch({type:"showMenu"})}>Add</button>
         </div>
     )
