7de58d8dd6cd4b2f7a010df7f137b65fe9bf51e0
JoannaAbella <Joanna.Abella96@gmail.com>
2019-05-06 13:57:25 +0200

    Commit before doing a git pull

--- amazingbox/src/components/addFileAndFolder.js
+++ amazingbox/src/components/addFileAndFolder.js
@@ -62,7 +62,7 @@ let AddFileButton = (props)=>{
             {state.showCreateFolder? 
             <div className="modal__shadow">
                 <div className="modal__shadow__container">
-                    <input type="text" onChange={(e)=> {state.inputValue = e.target.value; console.log(state.inputValue)}} required></input>
+                    <input className="modal__shadow__input" type="text" onChange={(e)=> {state.inputValue = e.target.value; console.log(state.inputValue)}} required></input>
                     <span className="modal__shadow__container__buttonBox">
                         <button className="modal__shadow__container__buttonBox__button" onClick={createFolder}>Create</button>
                         <button className="modal__shadow__container__buttonBox__button" onClick={()=>dispatch({type: "cancel_folder"})}>Cancel</button>
@@ -70,7 +70,9 @@ let AddFileButton = (props)=>{
                 </div>
             </div> : null}
 
-            {state.showMenu ? <div className="add-popupMenu"><input className="add-fileButton material-icons" type="file" multiple onChange={onFileChange}></input><button className="add-folderButton material-icons" onClick={()=>dispatch({type: "show_create_folder"})}>create_new_folder</button></div>: null}
+            {state.showMenu ? <div className="add-popupMenu"><input className="add-fileButton material-icons" type="file" multiple onChange={onFileChange}></input>   
+            <label className="custom-file-label material-icons" htmlFor="inputGroupFile01">insert_drive_file</label>
+            <button className="add-folderButton material-icons" onClick={()=>dispatch({type: "show_create_folder"})}>create_new_folder</button></div> : null}
             <button className="add-icon material-icons" onClick={()=>dispatch({type: "showMenu"})}>add_circle_outline</button>
         </div>
     )
