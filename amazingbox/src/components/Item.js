3a982987eca69183d21fd601d37ab39414e36773
JoannaAbella <Joanna.Abella96@gmail.com>
2019-05-07 15:14:11 +0200

    Implemented download function and added some styling

--- amazingbox/src/components/Item.js
+++ amazingbox/src/components/Item.js
@@ -3,15 +3,19 @@ import ItemMenu from "../components/ItemMenu/itemMenu";
 import { Link } from "react-router-dom";
 import Dropbox from "dropbox";
 import fetch from "isomorphic-fetch";
+import path from "./Path";
 
 import { getTime, bytesToSize } from "../utils";
 import {token$} from "../store/authToken"
+
+let dbx = new Dropbox.Dropbox({ fetch, accessToken: token$.value });
 
 function Item(props) {
     
     const [showMenu, updateShowMenu] = useState(false);
     const [image, updateImage] = useState("");
     const file = props.file;
+    const name = file.name;
     const type = file[".tag"];
 
     useEffect(() => {
@@ -25,8 +29,6 @@ function Item(props) {
     function getThumbnail(path) {
         console.log("image");
         console.log(path);
-
-        let dbx = new Dropbox.Dropbox({ fetch, accessToken: token$.value });
         dbx
           .filesGetThumbnail({path})
           .then(response => {  
@@ -52,12 +54,27 @@ function Item(props) {
         }
     }
 
+    function download() {
+        console.log("download");
+        console.log(path);
+        console.log(name);
+        
+        dbx
+          .filesGetTemporaryLink({path: `${file.path_lower}`})
+          .then(response => {
+              console.log(response)
+              window.location.href = response.link;
+          })
+          .catch(error => console.log(error));
+    }
+
     return (
         <>
             <td className="td-type material-icons">{type === "folder" ? "folder" : image}</td>
             <td className='td-name'>{type === "folder" ? <Link to={`/home${file.path_display}`}>{file.name}</Link> : file.name}</td>
             <td className='td-lastUpdate'>{type !== "folder" ? getTime(file.server_modified) : null}</td>
             <td className='td-size'>{type !== "folder" ? bytesToSize(file.size) : null}</td>
+            <td className="td-download material-icons">{type === "file" ? <span onClick={download}>cloud_download</span> : null}</td>
             <td className='td-menuButton'><button onClick={() => !showMenu ? updateShowMenu(true) : updateShowMenu(false)} className="td-menuButton material-icons">more_horiz</button></td>
             <td className='td-menu'>{showMenu ? <ItemMenu list={file} name={file.name} path={props.path} updateFiles={props.updateFiles} remove={props.remove}></ItemMenu> : null}</td>
         </>
