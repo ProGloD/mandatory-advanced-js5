9b0e807b6ad997f15ba587ecd43dd2d8448f6b25
JoannaAbella <Joanna.Abella96@gmail.com>
2019-05-07 13:23:13 +0200

    Pulling changes before continuing

--- amazingbox/src/components/ItemMenu/itemMenu.js
+++ amazingbox/src/components/ItemMenu/itemMenu.js
@@ -18,7 +18,7 @@ function ItemMenu(props) {
                 <button onClick={onClick} className="itemMenu-button openfirst" id="rename" >Rename</button>
                 <button onClick={onClick} className="itemMenu-button opensecond" id="move" >Move</button>
                 <button onClick={onClick} className="itemMenu-button openthird" id="remove" >Remove</button>
-                <button onClick={onClick} className="itemMenu-button openthird" id="copy" >Copy</button>
+                <button onClick={onClick} className="itemMenu-button openfourth" id="copy" >Copy</button>
 
             </div>
             {showPop ? <ShowPop list={props.list} name={props.name} path={props.path} showState={updateShowPop} updateFiles={props.updateFiles} className="showPop" sendId={id} remove={props.remove} /> : null}
