6ce187dcee4147af9da04e41e013f22bd07d0712
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-05-13 22:48:46 +0200

    added test for search clear and folder input

--- amazingbox/src/test/addFileAndFolder.test.js
+++ amazingbox/src/test/addFileAndFolder.test.js
@@ -7,11 +7,19 @@ import Adapter from 'enzyme-adapter-react-16';
 import { AssertionError } from 'assert';
 
 Enzyme.configure({ adapter: new Adapter() });
+const wrapper = shallow(<AddFileAndFolder/>)
 
 it('test add popup', () => {
-    const wrapper = shallow(<AddFileAndFolder/>)
     expect(wrapper.find(".add-popupMenu").exists()).toEqual(false);
     wrapper.find(".add-button").simulate("click");
     expect(wrapper.find(".add-popupMenu").exists()).toEqual(true);
 });
+
+it("test input for folder name", ()=>{
+    wrapper.find(".add-button").simulate("click");
+    wrapper.find(".add-folderButton").simulate("click");
+    expect(wrapper.find(".modal__shadow__input").exists()).toEqual(true);
+    wrapper.find(".modal__shadow__input").simulate("change", {target:{value: "testing folder input"}});
+    //check input value
+})
 
--- amazingbox/src/test/search.test.js
+++ amazingbox/src/test/search.test.js
@@ -7,12 +7,17 @@ import Adapter from 'enzyme-adapter-react-16';
 import { AssertionError } from 'assert';
 
 Enzyme.configure({ adapter: new Adapter() });
+const wrapper = shallow(<Search/>)
 
-it('test search value', () => {
-    const wrapper = shallow(<Search/>)
+it('test search value to be true', () => {
     let text = "test"
     expect(wrapper.find(".search-bar__text-input").exists()).toEqual(true);
     wrapper.find(".search-bar__text-input").simulate("change", {target:{value: text}});
-    expect(wrapper.find(".search-bar__text-input").value).toBe("test");
+    expect(wrapper.find(".search-bar__text-input").value).toBe("test");   //får undefined, kolla med andreas
 });
 
+it("test clear input", ()=>{
+    wrapper.find(".search-bar__text-input").simulate("change", {target:{value: "testing clear"}})
+    wrapper.find(".search-bar__clear-button").simulate("click");
+    expect(wrapper.find(".search-bar__text-input").value).toBe("");
+})
