da8f29053d801c68e8cadcf74a9ef221447c6954
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-05-14 12:20:41 +0200

    started styling login

--- amazingbox/src/components/login.js
+++ amazingbox/src/components/login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
-
+import "../css/login.css"
 import { login } from "../utils";
 import { token$ } from "../store/authToken";
 
@@ -12,18 +12,18 @@ function Login() {
   let auth = login();
 
   return (
-    <>
-      <a href={auth}>
-        <button>
-          Login using Dropbox{" "}
-          <img
+    <div className="login">
+              <img
             src="/images/Dropbox_Windows.png"
             width={24}
             alt="Dropbox Logo"
           />
+      <a href={auth} className="login_link">
+        <button className="login_link_button">
+          Login using Dropbox{" "}
         </button>
       </a>
-    </>
+    </div>
   );
 }
 
